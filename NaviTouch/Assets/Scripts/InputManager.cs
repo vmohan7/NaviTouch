15888b31141d41fbf30530245d91cc7805e52c0f
Vasanth Mohan <vmohan7@stanford.edu>
2015-10-18 00:31:17 -0700

    Add prefab and update README slightly

--- NaviTouch/Assets/Scripts/InputManager.cs
+++ NaviTouch/Assets/Scripts/InputManager.cs
@@ -1,4 +1,22 @@
-﻿using UnityEngine;
+﻿/*
+ * This file is part of NaviTouch.
+ * Copyright 2015 Vasanth Mohan. All Rights Reserved.
+ * 
+ * NaviTouch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NaviTouch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NaviTouch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NaviTouch
-Using Navi, an app that transforms your smartphone into a controller for VR, as a way to provide text input for VR
+Using Navi, an app that transforms your smartphone into a controller for VR, as a way to provide text input for VR.
+
+If you are intersted in using this demo for your applications, you can export the project and use the Keyboard Prefab located in the Prefabs folder. 
